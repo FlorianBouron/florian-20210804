792dc44683b0b82402d7f2eaad4023c115d4144d
Florian Bouron <bouron.florian@yahoo.fr>
2021-07-26 11:52:44 +0200

    Add hook useSocket and add it to OrderBook

--- src/components/organisms/OrderBook/OrderBook.tsx
+++ src/components/organisms/OrderBook/OrderBook.tsx
@@ -1,7 +1,16 @@
+/* eslint-disable no-console */
 import { Grid } from '@material-ui/core';
 import Table from '../../molecules/Table';
+import useSocket from '../../../hooks/useSocket';
+import { endpoint, message } from '../../../constants/socket';
 
 export default function OrderBook(): JSX.Element {
+  const { closeSocket } = useSocket({
+    url: endpoint,
+    message,
+    onError: (_message: string) => console.log(_message),
+    onMessage: (res: MessageEvent) => console.log(JSON.parse(res.data)),
+  });
   return (
     <Grid container>
       <Grid item xs={12} sm={6}>
---
+++ src/constants/socket.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+export const endpoint = 'wss://www.cryptofacilities.com/ws/v1';
+export const message = {
+  feed: 'book_ui_1',
+  product_ids: ['PI_XBTUSD'],
+};
