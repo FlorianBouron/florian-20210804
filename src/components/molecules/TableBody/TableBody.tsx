92889c4d42d66a1e8e5293c06ea52bbfe933eb20
Florian Bouron <bouron.florian@yahoo.fr>
2021-07-27 23:55:06 +0200

    Add proper color on price

--- src/components/molecules/TableBody/TableBody.tsx
+++ src/components/molecules/TableBody/TableBody.tsx
@@ -1,6 +1,7 @@
 import { TableRow, TableBody as TableBodyMaterial, TableCell } from '@material-ui/core';
 import { useOrders } from '../../../contexts/OrdersContext';
 import TableSideType from '../../../enums/tableSideType';
+import { green, red } from '../../../constants/colors';
 
 type TableBodyProps = {
   type: TableSideType;
@@ -15,13 +16,25 @@ export default function TableBody({ type }: TableBodyProps): JSX.Element {
         const [price, size, total] = row;
         return (
           <TableRow key={`${type}-${price}`}>
-            <TableCell size="small" align="center">
+            <TableCell
+              size="small"
+              align="center"
+              style={{
+                color: type !== TableSideType.BIDS ? red : undefined,
+              }}
+            >
               {type === TableSideType.BIDS ? total : price}
             </TableCell>
             <TableCell size="small" align="center">
               {size}
             </TableCell>
-            <TableCell size="small" align="center">
+            <TableCell
+              size="small"
+              align="center"
+              style={{
+                color: type === TableSideType.BIDS ? green : undefined,
+              }}
+            >
               {type === TableSideType.BIDS ? price : total}
             </TableCell>
           </TableRow>
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,6 +1,9 @@
 export const white = '#FFFFFF';
 export const purple = '#5741d9';
 export const red = '#b91d1d';
+export const redDark = '#3c1d27';
 export const gray = '#28303e';
 export const garyLight = '#424856';
 export const grayDark = '#121723';
+export const green = '#299065';
+export const greenDark = '#113534';
